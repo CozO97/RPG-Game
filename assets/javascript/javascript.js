bc2bcc40c93177b01fd0402af21cd4ba776b9c5e
Cosmin Sofletea <cozo@Cosmins-MBP.home>
2017-05-06 16:33:40 -0400

    works fine

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,32 +1,52 @@
 var RandomNumber;
-var gemNumber = [];
+var gem1;
+var gem2;
+var gem3;
+var gem4;
 var total = 0;
 var winCounter = 0;
 var lossCounter = 0;
 
-$(document).ready(function() {
 
-    RandomNumber = Math.floor(Math.random() * 50) + 49;
+function getRandomNumber() {
+    randomNumber = 0;
+    RandomNumber = Math.floor(Math.random() * 101) + 19;
     //console.log(RandomNumber);
     $('#randomNumGen').text(RandomNumber);
-
-});
-
-for (i = 0; i < 4; i++) {
-    gemNumber[i] = Math.floor(Math.random() * 11) + 1;
 }
-//gemNumber.push(gemNumber[i]);
-//console.log(gemNumber);
+getRandomNumber();
+
+function randomGemValue() {
+    gem1 = Math.floor((Math.random() * 12) + 1);
+    gem2 = Math.floor((Math.random() * 12) + 1);
+    gem3 = Math.floor((Math.random() * 12) + 1);
+    gem4 = Math.floor((Math.random() * 12) + 1);
+    //console.log(gem1);
+    //console.log(gem2);
+    //console.log(gem3);
+    //console.log(gem4);
+    $('.gem1').val(gem1);
+    //console.log(gem1);
+    $('.gem2').val(gem2);
+    //console.log(gem2);
+    $('.gem3').val(gem3);
+    //console.log(gem3);
+    $('.gem4').val(gem4);
+    //console.log(gem4);
+}
+randomGemValue();
+
+function reset() {
+    getRandomNumber();
+    randomGemValue();
+    total = 0;
+    $('.myNumber').html(total);
+    //sending value to html
+
+}
 
 
-$('.gem1').val(gemNumber[0]);
-$('.gem2').val(gemNumber[1]);
-$('.gem3').val(gemNumber[2]);
-$('.gem4').val(gemNumber[3]);
-console.log(gemNumber[0]);
-console.log(gemNumber[1]);
-console.log(gemNumber[2]);
-console.log(gemNumber[3]);
+
 
 total = 0;
 $('.myNumber').html(total);
@@ -41,11 +61,18 @@ $('button').click(function() {
         $("#wins").html("Wins: " + winCounter);
         //console.log(winCounter);
         alert("You have Won!!!");
+        setTimeout('reset()', 2000);
+
     }
     if (total > RandomNumber) {
         lossCounter++;
         $('#losses').html("Losses: " + lossCounter);
-        alert("Game Over! Try Again!")
-        setInterval('window.location.reload()', 3000);
+        alert("Game Over! Try Again!");
+        setTimeout('reset()', 2000);
+
     }
 });
+
+//1)Hard worker, punctual person, if I dont know thw answer to something I will research it till i find the answer that I am looking for. 
+//2) 
+//3)
